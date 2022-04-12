de6a176e72a6cacbcde2e31b0b4d16d4a7351590
smil-thakur <smil.raj.thakur@gmail.com>
2022-04-12 18:21:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,182 @@
+
+const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('.score')
+const blockWidth = 100
+const blockHeight = 20
+const ballDiameter = 20
+let timerId
+
+let xDirection = 2
+let yDirection = 2
+let score = 0;
+
+const userStart = [230, 10]
+const ballStart = [270, 40]
+let ballCurrentPosition = ballStart
+let currentPosition = userStart
+
+
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
+
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+
+]
+//draw my block
+function addBlock() {
+
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
+        grid.appendChild(block)
+    }
+}
+
+addBlock()
+
+//add user
+
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px'
+    user.style.bottom = currentPosition[1] + 'px'
+}
+
+//move user
+function moveUser(e) {
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10
+                drawUser()
+            }
+            break;
+        case 'ArrowRight':
+            if (currentPosition[0] < 460) {
+                currentPosition[0] += 10
+                drawUser()
+            }
+            break;
+
+    }
+}
+
+document.addEventListener('keydown', moveUser)
+//draw ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+//add ball
+
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+//move ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollisions()
+}
+timerId = setInterval(moveBall, 10)
+
+//check for collision
+function checkForCollisions() {
+
+
+    //check for user collisions
+    if
+        (
+        (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0] < currentPosition[0] + blockWidth) &&
+        (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1] < currentPosition[1] + blockHeight)
+    ) {
+        changeDirection()
+    }
+
+
+
+    for (let i = 0; i < blocks.length; i++) {
+        if
+            (
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ) {
+            const allblocks = Array.from(document.querySelectorAll('.block'))
+            allblocks[i].classList.remove('block')
+            blocks.splice(i, 1)
+            changeDirection()
+            score++
+            scoreDisplay.innerHTML = score
+            console.log(allblocks)
+        }
+    }
+    if (blocks.length == 0) {
+        scoreDisplay.innerHTML = 'You Win!'
+        clearInterval(timerId)
+        document.removeEventListener('keydown', moveUser)
+    }
+
+
+
+    if (ballCurrentPosition[0] >= (560 - ballDiameter) || ballCurrentPosition[1] >= (300 - ballDiameter)) {
+        changeDirection()
+    }
+
+    //check for gaveover
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = 'game over!'
+        document.removeEventListener('keydown', moveUser)
+    }
+
+}
+
+function changeDirection() {
+    if (xDirection === 2 && yDirection === 2) {
+        yDirection = -2
+        return
+    }
+    if (xDirection === 2 && yDirection === -2) {
+        xDirection = -2
+        return
+    }
+    if (xDirection === -2 && yDirection === -2) {
+        yDirection = 2
+        return
+    }
+    if (xDirection === -2 && yDirection === 2) {
+        xDirection = 2
+        return
+    }
+}
